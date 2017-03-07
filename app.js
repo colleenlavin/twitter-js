4f656e954fefc92cdd4a3b923f921fd27f58861f
colleenlavin <cml388@mun.ca>
2017-03-07 11:13:26 -0500

    Adds more functionality

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const path= require('path');
 const app = express();
-app.engine('html',nunjucks.render);
-app.set('view engine','html');
-nunjucks.configure('views')
-var locals = {
-    title: 'An Example',
-    people: [
-        { name:" Gandalf" },
-        { name:" Frodo" },
-        { name:" Hermione" },
-    ]
-}
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html',locals, function(err, output) {
-    console.log(output);
-});
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', { noCache: true })
 
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name:" Gandalf" },
+//         { name:" Frodo" },
+//         { name:" Hermione" },
+//     ]
+// }
+// nunjucks.configure('views', { noCache: true });
+// nunjucks.render('index.html',locals, function(err, output) {
+//     console.log(output);
+// });
+
+//app.use(express.static(path.join(__dirname, '../public')))
 app.use('/', function (req, res, next) {
     console.log('Request Type:', req.method)
     console.log('Request URL:', req.originalUrl)
@@ -28,6 +31,10 @@ app.use('/special/', function (req, res, next) {
 
     next()
 
+})
+app.get('/', function (req, res) {
+    const people = [{ name: 'Puppy1' }, { name: 'PuppiesRGr8' }, { name: 'iluvpupz' }];
+    res.render('index', { title: 'Twitter', people: people });
 })
 app.get('/special', function (req, res) {
     res.send('You have got puppies!')
